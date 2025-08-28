ef7df0d5f93308bc216f8553225a21531a97fcc8
wagnerjunioramos-wq <wagnerjunioramos@gmail.com>
2025-08-28 11:33:56 -0300

    add ola mundo

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,5 @@
-class OlaMundo {
-public static void main (String[] args){
-	System.out.println("Olá, mundo!");
+public class OlaMundo {
+	public static void main(string[] args){
+		System.out.println("Olá, mundo!");
 	}
 }
